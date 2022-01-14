268d1fec041e0e887f9d30c0b167b546299b3102
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-14 16:37:08 +0900

    Modify: 행렬의 덧셈 map으로 풀기

--- programmers/level1/행렬의-덧셈.js
+++ programmers/level1/행렬의-덧셈.js
@@ -1,11 +1,3 @@
 function solution(arr1, arr2) {
-  let answer = [];
-  for (let i = 0; i < arr1.length; i++) {
-    let temp = [];
-    for (let j = 0; j < arr1[0].length; j++) {
-      temp.push(arr1[i][j] + arr2[i][j]);
-    }
-    answer.push(temp);
-  }
-  return answer;
+  return arr1.map((a, row) => a.map((b, col) => b + arr2[row][col]));
 }
