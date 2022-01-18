34abd22bdd77ffeeb72061dd652bbaf60b8e4a75
Lami <halam.lee.lami@gmail.com>
2022-01-18 22:53:05 +0900

    10951: A+B - 4

---
+++ boj/level1/10951.js
@@ -0,0 +1,24 @@
+/*********************************************************************
+ *Title : A+B - 4
+ *Number : <img src="https://d2gd6pc034wcta.cloudfront.net/tier/3.svg" class="solvedac-tier">&nbsp;10951번
+ *Author : sandoll99
+ *Description : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ *Input : 입력은 여러 개의 테스트 케이스로 이루어져 있다.각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ *Output : 각 테스트 케이스마다 A+B를 출력한다.
+ *Start Time : 2022년 0월 18일 22시 41분 15초
+ *End Time : 2022년 0월 18일 시 분 초
+ *comments program : https://chrome.google.com/webstore/detail/boj-blog-post/ehhcjhfkaiohgflkjifmageahncijacd?hl=ko
+ *********************************************************************/
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+function solution() {
+  for (let i = 0; i < input.length - 1; i++) {
+    let numbers = input[i].split(' ');
+
+    console.log(Number(numbers[0]) + Number(numbers[1]));
+  }
+}
+
+solution();
