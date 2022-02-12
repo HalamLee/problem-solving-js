4d5e005b726692c9d6672e61dd097dc48d6b83fd
Lami <Halam.Lee.Lami@gmail.com>
2022-02-12 23:15:32 +0900

    boj level1 - AxB

---
+++ boj/level1/10998.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+if (input[input.length - 1] === '') input.pop();
+
+function solution() {
+  let num = input[0].split(' ');
+  console.log(Number(num[0]) * Number(num[1]));
+}
+
+solution();
