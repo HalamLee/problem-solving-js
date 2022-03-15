54c3c43c2c4d6c91fdce586049d85945c20126f1
Lami <Halam.Lee.Lami@gmail.com>
2022-03-15 23:52:03 +0900

    programmers level1 - 제일 작은 수 제거하기

---
+++ programmers/level1/제일-작은-수-제거하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+  const answer = [];
+  arr.forEach((el) => {
+    if (el !== Math.min(...arr)) {
+      answer.push(el);
+    }
+  });
+  if (arr.length === 1) {
+    answer.push(-1);
+  }
+  return answer;
+}
