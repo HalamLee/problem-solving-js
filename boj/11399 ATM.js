d36f5f050bbc3c1fdac2bf226009723100db8bdc
halamlee <halam.lee.lami@gmail.com>
2023-03-24 16:34:06 +0900

    :zap: 이중 for문 제거

--- boj/11399 ATM.js
+++ boj/11399 ATM.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 let [n, times] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 let answer = 0;
+let summary = 0;
 
 times = times
   .split(' ')
@@ -8,8 +9,7 @@ times = times
   .sort((a, b) => a - b);
 
 for (let i = 0; i < n; i++) {
-  for (let j = 0; j <= i; j++) {
-    answer += times[j];
-  }
+  summary += times[i];
+  answer += summary;
 }
 console.log(answer);
